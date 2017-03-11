7156be824170f958d4e1b56df9c34549e077e8c8
corysterner <corysterner@gmail.com>
2017-03-10 16:28:26 -0800

    set as tcp

--- netcat.js
+++ netcat.js
@@ -2,21 +2,27 @@ var Netcat = require('node-netcat');
 var PORT = 5025;
 var HOST = '131.243.171.207';
 
-var client = Netcat.udpClient(PORT, HOST);
-client.start();
-sendMessage();
+new Netcat.client(PORT, HOST)
+
+// client init connection
+
 
 client.on('open', function () {
 	console.log('open');
+	sendMessage();
 });
-client.on('message', function (msg, info, family){
-	console.log("received" + msg + "from" + info.address + ":" + info.port);
 
+client.on('error', function (err) {
+  console.log(err);
+}); 
+
+client.on('close', function () {
+  console.log('close');
 });
 
 function sendMessage(){
-	client.send('*IDN?');
+	client.send('DISP:TEXT "Working"');
 	console.log("Message sent");
 }
-
+client.start()
 
