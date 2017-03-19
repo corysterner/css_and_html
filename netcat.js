48f9b78e377a18c402a53e30776a5b2fe99237ef
corysterner <corysterner@gmail.com>
2017-03-19 15:33:02 -0700

    Update netcat.js

--- netcat.js
+++ netcat.js
@@ -2,8 +2,13 @@ var Netcat = require('node-netcat');
 var PORT = 5025;
 var HOST = '131.243.171.207';
 
-var client = Netcat.client(PORT, HOST);
-
+var client = Netcat.client(PORT, HOST, options);
+options = {
+ // define a connection timeout
+	timeout: 60000,
+ // buffer(default, to receive the original Buffer objects), ascii, hex,utf8, base64
+  read_encoding: 'ascii'
+ }
 // client init connection
 var myCallBack = function (err, data){
 	if (err) throw err;
